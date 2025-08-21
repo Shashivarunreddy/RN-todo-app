642609fbfff2526e0499bb7a7685673520689d91
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-08-21 16:31:40 +0530

    added UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,88 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import TodoInput from "@/components/TodoInput";
 import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
 import { useTheme } from "@/hooks/useTheme";
-import { useMutation, useQuery } from "convex/react";
+import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "convex/react";
 
-import { Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  FlatList,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-
+type Todo = Doc<"todos">;
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
-
+  const { colors } = useTheme();
+  const homestyles = createHomeStyles(colors);
   const todos = useQuery(api.todos.getTodos);
-console.log(todos?.length);
+  const isLoading = todos === undefined;
+  if (isLoading) return <LoadingSpinner />;
 
- const addtodo = useMutation(api.todos.addTodo);
-const deletealltodo = useMutation(api.todos.clearAllTodos);
+  const renderTodoItem = ({ item }: { item: Todo }) => {
+    return (
+      <View style={homestyles.todoItemWrapper}>
+        <LinearGradient
+          colors={colors.gradients.surface}
+          style={homestyles.todoItem}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+        >
+          <TouchableOpacity
+            style={homestyles.checkbox}
+            activeOpacity={0.7}
+            onPress={() => {}}
+          >
+            <LinearGradient
+              colors={
+                item.isCompleted
+                  ? colors.gradients.success
+                  : colors.gradients.muted
+              }
+              style={[
+                homestyles.checkboxInner,
+                {
+                  borderColor: item.isCompleted ? "transparent" : colors.border,
+                },
+              ]}
+            >
+              {item.isCompleted && (
+                <Ionicons name="checkmark" size={18} color="#fff" />
+              )}
+            </LinearGradient>
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
+    );
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#ea1414ff",
-       
-      }}
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homestyles.container}
     >
-      <Text>Hello world</Text>
-      <Text>Welcome to the app!</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>toggle mode</Text>
-      </TouchableOpacity>
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homestyles.safeArea}>
+        <Header />
+        <TodoInput />
 
-      <TouchableOpacity onPress={() => addtodo({ title: "New Todo" })}>
-        <Text>add todo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => deletealltodo()}>
-        <Text>delete all the  todo</Text>
-      </TouchableOpacity>
-    </View>
+        <FlatList
+          data={todos}
+          renderItem={renderTodoItem}
+          keyExtractor={(item) => item._id}
+          style={homestyles.todoList}
+          contentContainerStyle={homestyles.todoListContent}
+        />
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
+``
